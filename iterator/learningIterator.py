5592b4e439c9dbc48107f9be468bfa91aa20d3ab
PrateekJain14 <prateekjain14feb@gmail.com>
2018-11-11 00:41:15 +0530

    changed file

---
+++ iterator/learningIterator.py
@@ -0,0 +1,17 @@
+# this is how computer works when we run for loop
+# the same can be done using for loop as
+# for char in string :
+#       print(char)
+# creating an iterator and next function is used to move the iterator
+string = "123456789"
+my_iterator = iter(string)
+print(my_iterator)
+print(next(my_iterator))
+print(next(my_iterator))
+print(next(my_iterator))
+print(next(my_iterator))
+print(next(my_iterator))
+print(next(my_iterator))
+print(next(my_iterator))
+print(next(my_iterator))
+print(next(my_iterator))

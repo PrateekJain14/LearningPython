58f7248a75ddd467da35df9afd0f28493d63bf10
PrateekJain14 <prateekjain14feb@gmail.com>
2018-11-20 11:21:03 +0530

    Function2 added

--- funtion/demo.py
+++ funtion/demo.py
@@ -15,10 +15,28 @@ def python_food():
 python_food()
 
 
-def centre_text(text):
-    text= str(text)
+def centre_text(*args):
+    text= ""
+    for arg in args:
+        text += str(arg) + " "
     left_margin= (80-len(text)) // 2
     print(" "*left_margin, text)
 
 
 centre_text("HELLO WOrLD")
+centre_text("Spam", "Eggs", 34, 12)
+
+
+
+# PROGRAM TO MAKE OUR OWN PRINT FUNCTION
+
+def centre_text(*args, sep=' ', end='\n', file=None, flush=False):
+    text = ""
+    for arg in args:
+        text += str(arg) + sep
+    left_margin= (80-len(text)) // 2
+    print(" "*left_margin, text, end=end, file=file, flush=flush)
+
+with open("centeredFile", mode='w') as centered_file:
+    centre_text("HELLO WOrLD", file=centered_file)
+    centre_text("Spam", "Eggs", 34, 12, file=centered_file)

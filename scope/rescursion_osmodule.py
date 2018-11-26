b681932e42eefde9554cdd4546c20499d45c3a5e
PrateekJain14 <prateekjain14feb@gmail.com>
2018-11-26 14:00:04 +0530

    Recursion

---
+++ scope/rescursion_osmodule.py
@@ -0,0 +1,36 @@
+import os
+
+
+def list_directories(s):
+
+    # FUNCTION WITHIN FUNCTION
+    def dir_list(d):
+        nonlocal tab_stop
+        files = os.listdir(d)
+        for f in files:
+            current_dir = os.path.join(f + d)
+            if os.path.isdir(current_dir):
+                print("\t"* tab_stop + "directory " + f)
+                tab_stop += 1
+                dir_list(current_dir)
+                tab_stop -= 1
+            else:
+                print("\t"*tab_stop + f)
+
+    tab_stop = 0
+    if os.path.exists(s):
+        dir_list(s)
+    else:
+        print(s + "does not exist!")
+
+
+#
+# listing = os.walk('.')
+# for root, directories, files in listing:
+#     print(root)
+#     for d in directories:
+#         print(d)
+#         for file in files:
+#             print(file)
+
+list_directories(".")
--- scope/scope.py
+++ scope/scope.py
@@ -14,5 +14,26 @@ def factorial(n):
     else:
         return n * factorial(n-1)
 
-for i in range(100):
-    print(i, factorial(i))
+def fib(n):
+    if n < 2:
+        return n
+    else:
+        return fib(n-1) + fib(n-2)
+
+
+def fibonaci(n):
+    if n == 0:
+        result = 0
+    elif n == 1:
+        result = 1
+    else:
+        a = 0
+        b = 1
+        for f in range(1, n):
+            result = a + b
+            a = b
+            b = result
+    return result
+for i in range(36):
+    print(i, fibonaci(i))
+

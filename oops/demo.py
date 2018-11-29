fb914e0c3eb73f0fdc661ba4848631cbff33a681
PrateekJain14 <prateekjain14feb@gmail.com>
2018-11-29 17:44:11 +0530

    class third commit

---
+++ oops/demo.py
@@ -0,0 +1,8 @@
+first = "this is \n a string split \t\t and tabbed"
+print(first)
+
+raw_string = r"this is \n a string split \t\t and tabbed"
+print(raw_string)
+
+second = "this is" + chr(10) + "a string split" + chr(9) + chr(9) + "and tabbed"
+print(second)

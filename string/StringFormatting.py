0b28a162f0487743966dff817d2048a6bad5cb91
PrateekJain14 <prateekjain14feb@gmail.com>
2018-12-08 17:25:58 +0530

    STRING updated

--- string/StringFormatting.py
+++ string/StringFormatting.py
@@ -5,6 +5,7 @@ print("my age is {0}".format(age))
 print("there are {0} days in {1}, {2}, {3}, {4}, {5}, {6}, {7}".format(31,"january","march","may","july","august","october","december"))
 # {0} -> they represent replacement of data at a particular possition
 
+
 print("""January : {0}
  February : {2}
  march : {0}
